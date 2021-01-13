c490ea9e72700d69e4488701d302d4b1877388c7
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-13 13:10:03 -0600

    add a form to create a personalized letter special for you

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    const nameInput = $("input#name").val();
+    $(".name").text(nameInput);
+    $("#letter").show();
+    event.preventDefault();
+  });
+});
